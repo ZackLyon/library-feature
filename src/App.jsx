7e67a910a78e84fd25e6799a106d52824519d90e
Zack Lyon <zack.j.lyon@gmail.com>
2021-12-06 14:09:56 -0800

    Add react router to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
+import Home from './views/Home/Home'
 import './App.css'
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Router>
+        <div>
+          <nav>
+            <div>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </div>
+            <div>
+              <Link className="nav-link" to="/books">
+                Books
+              </Link>
+            </div>
+          </nav>
+
+          <Switch>
+            <Route path="/books/:id">
+              <BookDetail />
+            </Route>
+            <Route path="/books">
+              <h1>Library Catalog</h1>
+              <BookList />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </main>
   )
 }
